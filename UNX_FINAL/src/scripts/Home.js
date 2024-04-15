5a77d7497fb0e93cee66edc8857db570f99676e7
LauraC360 <63462151+LauraC360@users.noreply.github.com>
2024-04-15 22:39:50 +0300

    update_added_pages_Laura
    versiune finala 1

---
+++ UNX_FINAL/src/scripts/Home.js
@@ -0,0 +1,12 @@
+function navigateToLogin() {
+  window.location.href = "login.html";
+}
+
+function navigateToRegister() {
+  window.location.href = "register.html";
+}
+
+function navigateToUnemployment() {
+  window.location.href = "unemploymentData.html";
+}
+
